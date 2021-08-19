82a382a6a4f381e0719f2d57d3e57ea30f7455de
ailin lopez cortese <ailinlopezcortese.g@hotmail.com>
2021-08-18 22:56:17 -0300

    Cambios finales

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-
 const app = express();
 const puerto = 8080;
+
+const productos = [];
 
 const server = app.listen(puerto, () => {
     console.log("Servidor inicializado.");
@@ -10,10 +11,16 @@ const server = app.listen(puerto, () => {
 
 app.get('/api/productos', async (req, res) => {
     try {
-        const productos = await info;
-        res.json({
-            productos: productos
-        })
+        const info = await productos;
+        if(info !== []) {
+            res.json({
+                error: "No hay productos cargados"
+            })
+        } else {
+            res.json({
+                productos: info
+            })
+        }
     } catch (error) {
         console.log(error)
     }
@@ -21,15 +28,37 @@ app.get('/api/productos', async (req, res) => {
 
 app.get('api/productos/:id', async (req, res) => {
     try {
-        
+        if(req.params.id === productos[parseInt(req.params.id) - 1].id) {
+            const producto = await productos.filter(req.params.id === productos[parseInt(req.params.id) - 1].id);
+            res.json({
+                producto: producto
+            });
+        } else {
+            res.json({
+                error: "No hay un producto con el id ingresado."
+            })
+        }
     } catch (error) {
         console.log(error);
     }
 })
 
-app.post('api/productos', async (req, res) => {
+app.post('api/productos/cargar', async (req, res) => {
     try {
-        
+        const nombre = prompt("Ingrese nombre del producto.");
+        const precio = prompt("Ingrese precio del producto.");
+        const thumbnail = prompt("Ingrese el nombre de la imagen del producto.");
+        const id = productos.length() + 1;
+        const nuevoProducto = {
+            id: id,
+            nombre: nombre,
+            precio: precio,
+            thumbnail: thumbnail
+        }
+        productos.push(nuevoProducto);
+        res.json({
+            info: "El producto ha sido creado correctamente."
+        });
     } catch (error) {
         console.log(error)
     }
